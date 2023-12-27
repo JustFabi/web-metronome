9bd1897b68367deb9869da9589ceb85a0f4b0ccf
fabianmerkl <108922830+fabianmerkl@users.noreply.github.com>
2023-12-27 17:15:09 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import BpmInput from "./components/BpmInput.vue";
+</script>
+
+<template>
+  <div class="flex justify-center items-center h-screen">
+    <BpmInput bpm="100"></BpmInput>
+  </div>
+</template>
---
+++ src/components/BpmInput.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from "vue";
+const props = defineProps({
+  bpm: Number,
+});
+const bpm = ref(props.bpm);
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-4">
+    <h2 class="text-4xl">{{ bpm }}</h2>
+    <input v-model="bpm" type="range" min="0" max="400" />
+  </div>
+</template>
